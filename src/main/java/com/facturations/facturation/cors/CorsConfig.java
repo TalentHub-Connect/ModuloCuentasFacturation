8ee89e4beb29f19fca543d35018ede46b167cac1
ticaso <ticaso999@gmail.com>
2024-04-30 23:01:50 -0500

    Changes

--- src/main/java/com/facturations/facturation/cors/CorsConfig.java
+++ src/main/java/com/facturations/facturation/cors/CorsConfig.java
@@ -1,5 +1,4 @@
 package com.facturations.facturation.cors;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
