3ed5a377c3127e7377c5deff7c919a74cdd17ef1
ticaso <ticaso999@gmail.com>
2024-05-11 18:55:37 -0500

    changes for env

--- src/main/java/com/facturations/facturation/cors/CorsConfig.java
+++ src/main/java/com/facturations/facturation/cors/CorsConfig.java
@@ -1,12 +1,22 @@
 package com.facturations.facturation.cors;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
     }
 }
